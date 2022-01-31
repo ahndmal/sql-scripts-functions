20ec00b82744514c6ade7250ecb9f7308b7bbfe3
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2022-01-31 06:59:12 -0800

    Create alter.sql

---
+++ alter.sql
@@ -0,0 +1,22 @@
+ALTER TABLE table ADD COLUMN column data_type;
+
+--Similarly, we can remove a column:
+ALTER TABLE table DROP COLUMN column;
+
+--To change the data type of a column:
+ALTER TABLE table ALTER COLUMN column SET DATA TYPE data_type;
+
+--Adding a NOT NULL constraint to a column
+ALTER TABLE table ALTER COLUMN column SET NOT NULL;
+
+-- remove constraint
+ALTER TABLE table ALTER COLUMN column DROP NOT NULL;
+
+-- update one table with values from another table
+UPDATE table
+SET column = (SELECT column
+ FROM table_b
+ WHERE table.column = table_b.column)
+WHERE EXISTS (SELECT column
+ FROM table_b
+ WHERE table.column = table_b.column);

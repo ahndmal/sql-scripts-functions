ecec69535a8e76834b0a73d5aed7c53be1860964
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2022-01-31 07:04:35 -0800

    Update alter.sql

--- alter.sql
+++ alter.sql
@@ -20,3 +20,9 @@ SET column = (SELECT column
 WHERE EXISTS (SELECT column
  FROM table_b
  WHERE table.column = table_b.column);
+ 
+-- Creating a Column Copy
+ALTER TABLE meat_poultry_egg_inspect ADD COLUMN st_copy varchar(2);
+UPDATE meat_poultry_egg_inspect
+SET st_copy = st;
+
